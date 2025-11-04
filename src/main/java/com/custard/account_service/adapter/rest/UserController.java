266988af1a6074f65fea49c43320b72a3546234d
Proautotype <51104351+Proautotype@users.noreply.github.com>
2025-11-04 12:38:31 +0000

    added missing documentation to handlers

--- src/main/java/com/custard/account_service/adapter/rest/UserController.java
+++ src/main/java/com/custard/account_service/adapter/rest/UserController.java
@@ -1,16 +1,21 @@
 package com.custard.account_service.adapter.rest;
 
+import com.custard.account_service.adapter.dto.ProfileDto;
 import com.custard.account_service.adapter.dto.UserDto;
+import com.custard.account_service.adapter.dto.reponses.FailureApiResponse;
 import com.custard.account_service.adapter.dto.reponses.SuccessApiResponse;
 import com.custard.account_service.adapter.mapper.Adapter_UserMapper;
 import com.custard.account_service.application.commands.CreateProfileCommand;
 import com.custard.account_service.application.commands.CreateUserCommand;
 import com.custard.account_service.application.commands.UpdateUserCommand;
-import com.custard.account_service.application.usecases.CreateProfileUseCase;
-import com.custard.account_service.application.usecases.CreateUserUseCase;
-import com.custard.account_service.application.usecases.FindUserByIdUseCase;
-import com.custard.account_service.application.usecases.UpdateUserUseCase;
+import com.custard.account_service.application.usecases.*;
+import com.custard.account_service.domain.models.Profile;
 import com.custard.account_service.domain.models.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -30,8 +35,37 @@ public class UserController {
     private final UpdateUserUseCase updateUserUseCase;
     private final CreateProfileUseCase createProfileUsecase;
     private final FindUserByIdUseCase getUserUseCase;
+    private final FindProfileByUserIdUseCase findProfileByUserIdUseCase;
 
-    @PostMapping("/create-account")
+    @PostMapping("/create")
+    /**
+     * Creates a new user.
+     * @param command the create user command containing username and email.
+     * @return a response entity containing the created user.
+     */
+    @Operation(
+            method = "POST",
+            description = "Create a new user on the AccountService",
+            responses = {
+                    @ApiResponse(
+                            description = "Success with code 00",
+                            responseCode = "201"
+                    ),
+                    @ApiResponse(
+                            description = "User already exist with code 01",
+                            responseCode = "402"
+                    ),
+                    @ApiResponse(
+                            description = "Internal Server error with code 01",
+                            responseCode = "500",
+                            content = {
+                                    @Content(
+                                            schema = @Schema(implementation = FailureApiResponse.class)
+                                    )
+                            }
+                    )
+            }
+    )
     public ResponseEntity<SuccessApiResponse<UserDto>> create(@RequestBody CreateUserCommand command) {
         logger.info("Create user request received");
         User user = createUserUseCase.execute(command);
@@ -40,7 +74,26 @@ public class UserController {
         return ResponseEntity.status(201).body(successApiResponse);
     }
 
-    @GetMapping("/get-account/{userId}")
+    @GetMapping("/{userId}")
+    @Operation(
+            method = "GET",
+            description = "Get user details by userId",
+            responses = {
+                    @ApiResponse(
+                            description = "Success with code 00",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "User not found with code 01",
+                            responseCode = "404",
+                            content = {
+                                    @Content(
+                                            schema = @Schema(implementation = FailureApiResponse.class)
+                                    )
+                            }
+                    )
+            }
+    )
     public ResponseEntity<SuccessApiResponse<UserDto>> getAccountDetails(@PathVariable("userId") String userId) {
         logger.info("get user request received by id : {} ", userId);
         User user = getUserUseCase.execute(userId);
@@ -49,7 +102,7 @@ public class UserController {
         return ResponseEntity.status(201).body(successApiResponse);
     }
 
-    @PatchMapping("/update-account")
+    @PatchMapping("/update")
     public ResponseEntity<SuccessApiResponse<UserDto>> update(UpdateUserCommand command) {
         logger.info("Update user request received");
         User user = updateUserUseCase.execute(command);
@@ -59,6 +112,25 @@ public class UserController {
     }
 
     @PatchMapping("/profile")
+    @Operation(
+            method = "PATCH",
+            description = "Add user profile",
+            responses = {
+                    @ApiResponse(
+                            description = "Success with code 00",
+                            responseCode = "201"
+                    ),
+                    @ApiResponse(
+                            description = "User not found with code 01",
+                            responseCode = "404",
+                            content = {
+                                    @Content(
+                                            schema = @Schema(implementation = FailureApiResponse.class)
+                                    )
+                            }
+                    )
+            }
+    )
     public ResponseEntity<SuccessApiResponse<UserDto>> addUserProfile(@RequestBody CreateProfileCommand command) {
         logger.info("create profile user request received");
         User user = createProfileUsecase.execute(command);
@@ -68,11 +140,30 @@ public class UserController {
     }
 
     @GetMapping("/profile/{userId}")
-    public ResponseEntity<SuccessApiResponse<UserDto>> getProfileByUserId(CreateProfileCommand command) {
-        logger.info("create profile user request received");
-        User user = createProfileUsecase.execute(command);
-        SuccessApiResponse<UserDto> successApiResponse = new SuccessApiResponse<>();
-        successApiResponse.setData(adapterUserMapper.toUserDto(user));
+    @Operation(
+            method = "GET",
+            description = "Get user profile by user id",
+            responses = {
+                    @ApiResponse(
+                            description = "Success with code 00",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "User not found with code 01",
+                            responseCode = "404",
+                            content = {
+                                    @Content(
+                                            schema = @Schema(implementation = FailureApiResponse.class)
+                                    )
+                            }
+                    )
+            }
+    )
+    public ResponseEntity<SuccessApiResponse<ProfileDto>> getProfileByUserId(@PathVariable("userId") String userId) {
+        logger.info("Finding user profile by id -> {} ", userId);
+        Profile user = findProfileByUserIdUseCase.execute(userId);
+        SuccessApiResponse<ProfileDto> successApiResponse = new SuccessApiResponse<>();
+        successApiResponse.setData(adapterUserMapper.toProfileDto(user));
         return ResponseEntity.status(201).body(successApiResponse);
     }
 
